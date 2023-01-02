247a5ca953a661bc05a8f2ed4f7e71b544159ab9
Alok kumar <75192701+alok007kr@users.noreply.github.com>
2023-01-02 12:33:12 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ const userSchema = new Schema({
     name: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    role: {type: String, default: 'customer'}
+    role: {type: String, default: 'author'}
 }, {timestamps: true})
 
 
